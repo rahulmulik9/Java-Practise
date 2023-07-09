b29a69f5678d7c0b04ea995cd707ef7d25d82376
Rahul Mulik <rahulmulik9@gmail.com>
2023-07-09 18:41:47 +0530

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.print("First Commit");
+
+        TmpClass tmpclass = new TmpClass("Rahul is here");
+
+        tmpclass.showName();
+
     }
 }
---
+++ src/TmpClass.java
@@ -0,0 +1,19 @@
+public class TmpClass {
+    private String name;
+
+    public TmpClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void showName(){
+        System.out.println(name);
+    }
+}
