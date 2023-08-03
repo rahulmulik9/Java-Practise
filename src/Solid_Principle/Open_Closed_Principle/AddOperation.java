aa5f28f9e3fe7c03dd1e222f4aed13ca20c88d59
Rahul Mulik <rahulmulik9@gmail.com>
2023-08-03 20:38:55 +0530

    Solid Principle 1]Open-Closed Principle

---
+++ src/Solid_Principle/Open_Closed_Principle/AddOperation.java
@@ -0,0 +1,9 @@
+package Solid_Principle.Open_Closed_Principle;
+
+public class AddOperation implements Operation {
+
+    @Override
+    public int perform(int a, int b) {
+        return a+b;
+    }
+}
---
+++ src/Solid_Principle/Open_Closed_Principle/Operation.java
@@ -0,0 +1,6 @@
+package Solid_Principle.Open_Closed_Principle;
+
+public interface Operation {
+    public int perform(int a, int b);
+}
+
