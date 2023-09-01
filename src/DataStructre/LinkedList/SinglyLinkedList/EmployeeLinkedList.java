687c35c0da44464ce8faf798c536a3c99f59579b
Rahul Mulik <rahulmulik9@gmail.com>
2023-09-01 21:26:02 +0530

    Added singly Linked list delet

--- src/DataStructre/LinkedList/SinglyLinkedList/EmployeeLinkedList.java
+++ src/DataStructre/LinkedList/SinglyLinkedList/EmployeeLinkedList.java
@@ -3,16 +3,35 @@ package DataStructre.LinkedList.SinglyLinkedList;
 public class EmployeeLinkedList {
     private EmployeeNode head;
 
-    public void addToFront(Employee employee){
-        EmployeeNode node=new EmployeeNode(employee);
+    public void addToFront(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee);
         node.setNext(head);
         head = node;
     }
 
-    public void printList(){
-        EmployeeNode current =head;
+    public String deletFront() {
+        if (!isEmpty()) {
+            return null;
+        }
+        EmployeeNode removedNode =head ;
+        head = head.getNext();
+        removedNode.setNext(null);
+        return removedNode.toString();
+    }
+    public boolean isEmpty(){
+        if (head ==null) {
+            return false;
+        }else {
+
+            return true;
+        }
+
+    }
+
+    public void printList() {
+        EmployeeNode current = head;
         System.out.println("Head->");
-        while(current != null){
+        while (current != null) {
             System.out.println(current);
             current = current.getNext();
         }
