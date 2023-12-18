e664bbd43309ae2ff29d38a59c0431daa023a027
rahulmulik9 <rahulmulik9@gmail.com>
2023-12-18 20:01:29 +0530

    Data Types

---
+++ src/basic/b1_datatypes/App.java
@@ -0,0 +1,22 @@
+package basic.b1_datatypes;
+
+public class App {
+    public static void main(String[] args) {
+
+        int intmin=Integer.MIN_VALUE;
+        int intmax=Integer.MAX_VALUE;
+        System.out.println(intmin);
+        System.out.println(intmax);
+
+
+        byte bytemin = Byte.MIN_VALUE;
+        byte bytemax = Byte.MAX_VALUE;
+        System.out.println(bytemin);
+        System.out.println(bytemax);
+
+        short shortmin = Short.MIN_VALUE;
+        short shortmax = Short.MAX_VALUE;
+        System.out.println(shortmin);
+        System.out.println(shortmax);
+    }
+}
