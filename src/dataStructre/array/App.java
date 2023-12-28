d0167c8664c48a1c12ca97cdb0486ae2bc8714b9
rahulmulik9 <rahulmulik9@gmail.com>
2023-12-28 21:56:13 +0530

    Array

---
+++ src/dataStructre/array/App.java
@@ -0,0 +1,10 @@
+package dataStructre.array;
+
+public class App {
+    public static void main(String[] args) {
+        // int[] ary=new int[];   //=> wrong as you have to define size
+        int[] ary;   ary=new int[7];
+        int ary1[];
+        int[] ad={1,5,6};
+    }
+}
