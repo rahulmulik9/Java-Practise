534a81010969f048ef37a9036837de472b7b8004
rahulmulik9 <rahulmulik9@gmail.com>
2023-12-31 19:28:26 +0530

    Comments in singleton

--- src/design_Patterns/singleTon_Design/EagerSingleton.java
+++ src/design_Patterns/singleTon_Design/EagerSingleton.java
@@ -2,7 +2,7 @@ package design_Patterns.singleTon_Design;
 
 /**
  * This is eager initialization concept where
- * as soon as JVM start the object will be created irrespective whether it got accessed by
+ *  as soon as JVM start the object will be created (as it is static member) irrespective whether it got accessed by
  * any code in application or not.
  * When to use : One possible usage can be let say your application has some static cache which is required to be loaded.
  * Drawback : As mention consumes resource even if application does not use it.
--- src/design_Patterns/singleTon_Design/Singleton.java
+++ src/design_Patterns/singleTon_Design/Singleton.java
@@ -1,12 +1,18 @@
 package design_Patterns.singleTon_Design;
 
 public class Singleton {
+
+    //classs instance should be static
     private static Singleton instance;
     private String data;
 
+
+    //constructor must be private so no one access it
     private Singleton(String data){
         this.data = data;
     }
+
+
 //    public static Singleton getInstance(String data){
 //        if(instance != null){
 //        instance = new Singleton(data);}
@@ -16,7 +22,7 @@ public class Singleton {
     // two threads from accessing code at same time from above getInstance method code.
     //use synchronized
     // thread will wait until there turn comes up
-    //in this methos there is thread waiting
+    //in these methods there is thread waiting
     public static Singleton getInstance(String data) {
         synchronized (Singleton.class) {
             if (instance != null) {
