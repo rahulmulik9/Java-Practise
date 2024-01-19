f6c7107d4e5046bc5a4d522ad9df03ee7ce5377d
rahulmulik9 <rahulmulik9@gmail.com>
2024-01-19 23:44:18 +0530

    Simple example of generic using classes

---
+++ src/DataStructre/Generic/clasesUsingGeneric/ClsString.java
@@ -0,0 +1,2 @@
+package dataStructre.Generic.clasesUsingGeneric;
+
---
+++ src/DataStructre/Generic/clasesUsingGeneric/Main.java
@@ -0,0 +1,54 @@
+package dataStructre.Generic.clasesUsingGeneric;
+
+public class Main {
+    public static void main(String[] args) {
+        //using regular class
+        ClsInteger myInt=new ClsInteger(10);
+        ClsString myStr=new ClsString("Anbc");
+
+        System.out.println(myInt.getA());
+        System.out.println(myStr.getAb());
+
+        //using generic class
+        ClsGeneric myGINt=new ClsGeneric(10);
+        ClsGeneric myGStr=new ClsGeneric("abc");
+        System.out.println(myGINt.getAb());
+        System.out.println(myGStr.getAb());
+    }
+}
+
+class ClsInteger {
+    int a;
+
+    public ClsInteger(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+}
+class ClsString {
+    String ab;
+
+    public ClsString(String ab) {
+        this.ab = ab;
+    }
+
+    public String getAb() {
+        return ab;
+    }
+}
+
+class ClsGeneric<T>{
+    T ab;
+
+    public ClsGeneric(T ab) {
+        this.ab = ab;
+    }
+
+    public T getAb() {
+        return ab;
+    }
+}
+
