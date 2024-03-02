81eb3fae6ac2590c51c69f866e5238a09c5e91f0
Rahul Mulik <rahulmulik9@gmail.com>
2024-03-03 00:17:01 +0530

    Udemy class Lambda Expression

---
+++ src/basic/b2_OPPS_Concepts/lamdaExpression/Intro/FunctionalInterface/Main.java
@@ -0,0 +1,15 @@
+package basic.b2_OPPS_Concepts.lamdaExpression.Intro.FunctionalInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = calculator((a,b)->a+b,5,6);
+        int resultMulitply = calculator((a,b)->a*b,5,6);
+        System.out.println(result);
+        System.out.println(resultMulitply);
+
+    }
+    public static <T> T calculator(Operations<T> function, T value1, T value2){
+
+        return function.operate(value1,value2);
+    }
+}
---
+++ src/basic/b2_OPPS_Concepts/lamdaExpression/Intro/FunctionalInterface/Operations.java
@@ -0,0 +1,6 @@
+package basic.b2_OPPS_Concepts.lamdaExpression.Intro.FunctionalInterface;
+
+@FunctionalInterface
+public interface Operations <T>{
+    T operate(T value1,T value2);
+}
