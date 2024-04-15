e80d2703744719ac99fd3bb794ca87c9eeeb1f94
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-15 22:12:44 +0530

    Udemy class Stream : Added functionality for main class

--- src/basic/b7_stream/studentExample/Main.java
+++ src/basic/b7_stream/studentExample/Main.java
@@ -7,12 +7,10 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Main {
+public class Main {  public static void main(String[] args) {
 
-    public static void main(String[] args) {
-
-        Course pymc= new Course("PYMC", "Python Masterclass");
-        Course jmc= new Course("JMC", "Java Masterclass");
+    Course pymc= new Course("PYMC", "Python Masterclass");
+    Course jmc= new Course("JMC", "Java Masterclass");
 //        Student tim = new Student("AU", 2019, 30, "M",
 //                true, jmc, pymc);
 //        System.out.println(tim);
@@ -21,10 +19,38 @@ public class Main {
 //        tim.watchLecture("PYMC", 7, 7, 2020);
 //        System.out.println(tim);
 
-        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(10)
-                .forEach(System.out::println);
+//        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+//                .limit(10)
+//                .forEach(System.out::println);
 
+    Student[] students = new Student[1000];
+    Arrays.setAll(students, (i) -> Student.getRandomStudent(jmc, pymc));
 
+    var maleStudents = Arrays.stream(students)
+            .filter(s -> s.getGender().equals("M"));
+
+    System.out.println("# of male students " + maleStudents.count());
+
+    for (String gender : List.of("M", "F", "U")) {
+        var myStudents = Arrays.stream(students)
+                .filter(s -> s.getGender().equals(gender));
+        System.out.println("# of " + gender + " students " + myStudents.count());
     }
+
+    List<Predicate<Student>> list = List.of(
+            (s) -> s.getAge() < 30,
+            (Student s) -> s.getAge() >= 30 && s.getAge() < 60
+    );
+
+    long total = 0;
+    for (int i = 0; i < list.size(); i++) {
+        var myStudents = Arrays.stream(students).filter(list.get(i));
+        long cnt = myStudents.count();
+        total += cnt;
+        System.out.printf("# of students (%s) = %d%n",
+                i == 0 ? " < 30" : ">= 30 & < 60", cnt);
+    }
+    System.out.println("# of students >= 60 = " + (students.length - total));
+
+}
 }
