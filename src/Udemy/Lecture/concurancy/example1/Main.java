4a7c84223aac77bcc5b8592d3dfcd5bd73f05b83
Rahul <rahulmulik9@gmail.com>
2024-05-07 01:31:14 +0530

    Thread class simple example

---
+++ src/Udemy/Lecture/concurancy/example1/Main.java
@@ -0,0 +1,21 @@
+package Udemy.Lecture.concurancy.example1;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread  = java.lang.Thread.currentThread();
+        System.out.println(thread.getClass().getName());    //java.lang.Thread
+        System.out.println(thread.getName());                 //main
+
+        printThredState(thread);
+    }
+
+    public static void printThredState(Thread thread){
+        System.out.println("-----------------------------------");
+        System.out.println("Thread id: "+ thread.getId());
+        System.out.println("Thread Name : "+ thread.getName());
+        System.out.println("Priority : "+ thread+ thread.getPriority());
+        System.out.println("State : "+ thread.getState());
+        System.out.println("Is Alive : "+ thread.isAlive());
+        System.out.println("___________________________________");
+    }
+}
--- src/basic/b2_OPPS_Concepts/lamdaExpression/FunctionalInterface/Operations.java
+++ src/basic/b2_OPPS_Concepts/lamdaExpression/FunctionalInterface/Operations.java
@@ -3,4 +3,5 @@ package basic.b2_OPPS_Concepts.lamdaExpression.FunctionalInterface;
 @FunctionalInterface
 public interface Operations <T>{
     T operate(T value1,T value2);
+
 }
