56938a4272de4c0931b0b499c282cc72904a16f9
Rahul <rahulmulik9@gmail.com>
2024-05-12 07:37:19 +0530

    Concurrency Synchronization method Synchronization block

--- src/Udemy/Lecture/concurancy/example7_synchronization/sychronise_Block/BankAccount.java
+++ src/Udemy/Lecture/concurancy/example7_synchronization/sychronise_Block/BankAccount.java
@@ -12,27 +12,33 @@ public class BankAccount {
     }
 
 
-    public  void depositeNew(double amount){
-        try{
+    public void depositeNew(double amount) {
+        try {
             System.out.println("Deposit is happening");
             Thread.sleep(7000);
-        }catch (InterruptedException e){
-            throw  new RuntimeException();
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
         }
-        synchronized (this){
-            balance = balance+amount;
-            System.out.printf("Balance after deposite: "+balance);
+        synchronized (this) {
+            balance = balance + amount;
+            System.out.printf("Balance after deposite: " + balance);
         }
+       /* You can use object of any type of object here, I am using Double which is object type, so newBalance will not able to aquried by other thread
+        Double newBalance = this.balance;
+        synchronized (newBalance) {
+            balance = balance + amount;
+            System.out.printf("Balance after deposite: " + balance);
+        }*/
 
     }
 
 
-    public void withdrawNew(double amount){
-        try{
+    public void withdrawNew(double amount) {
+        try {
             System.out.println("Withdraw is happening");
             Thread.sleep(100);
-        }catch (InterruptedException e){
-            throw  new RuntimeException();
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
         }
         synchronized (this) {
             if (amount <= balance) {
