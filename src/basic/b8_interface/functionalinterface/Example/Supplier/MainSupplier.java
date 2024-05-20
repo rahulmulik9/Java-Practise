f62880a5209a7d5e9820ebd2d330fe9d3d85bd8a
Rahul <rahulmulik9@gmail.com>
2024-05-20 20:54:20 +0530

    Functional interface Supplier

---
+++ src/basic/b8_interface/functionalinterface/Example/Supplier/MainSupplier.java
@@ -0,0 +1,63 @@
+package basic.b8_interface.functionalinterface.Example.Supplier;
+
+import java.util.function.Supplier;
+interface OwnSupplier<T>{
+    public T get();
+}
+
+
+public class MainSupplier {
+    public static void main(String[] args) {
+        //own supplier class
+        OwnSupplier<String> ownSupplier = new OwnSupplier<String>() {
+            @Override
+            public String get() {
+                return "Rahul";
+            }
+        };
+
+        String name = ownSupplier.get();
+        System.out.println(name);
+
+
+        String name1 = new OwnSupplier<String>(){
+            @Override
+            public String get() {
+                return "Rahul";
+            }
+        }.get();
+        System.out.println(name1);
+
+
+        //using deafult supplier class
+        Supplier<String> defaultsupplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                return "Rahul";
+            }
+        };
+        String name2 =defaultsupplier.get();
+        System.out.println(name2);
+
+
+
+        String name4 = new Supplier<String>(){
+            @Override
+            public String get() {
+                return "Rahul";
+            }
+        }.get();
+
+        System.out.println(name4);
+
+
+
+
+        Supplier<String> supplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                return "Rahul";
+            }
+        };
+    }
+}
