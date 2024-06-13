01fc7c6a6e2385b6ecff2cb448f9d00f635de246
Rahul <rahulmulik9@gmail.com>
2024-06-13 22:18:02 +0530

    Multiline string using """asdf adfafd  """

--- src/basic/b1_datatypes/string/Main.java
+++ src/basic/b1_datatypes/string/Main.java
@@ -2,17 +2,25 @@ package basic.b1_datatypes.string;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Multiline string
+        String str= """
+                Hi my Name is Rahul. 
+                I am 28 years old
+                """;
+        System.out.println(str);
+
         String name = "Rahul";
-stringPatterMatching(name);
+        stringPatterMatching(name);
     }
 
     public static void stringPatterMatching(Object message) {
 
-       if(message instanceof String){
-           System.out.println("Instance of string but message.length is not accessible as it is Object message");
-       }
-       if(message instanceof String name){
-           System.out.println("Instance of string "+ name.length());
-       }
+        if (message instanceof String) {
+            System.out.println("Instance of string but message.length is not accessible as it is Object message");
+        }
+        if (message instanceof String name) {
+            System.out.println("Instance of string " + name.length());
+        }
     }
 }
