3e02ae7097de9b6d6f1aa9e8a7cd12d61008835c
Rahul <rahulmulik9@gmail.com>
2024-06-20 20:46:28 +0530

    Volatile vs synchronise

--- src/basic/b4_Concurancy/volatilevsAtomic/Main.java
+++ src/basic/b4_Concurancy/volatilevsAtomic/Main.java
@@ -40,3 +40,30 @@ public class Main {
 }
 
 
+/*If you run programme without volatile variable and synchronized method
+then output is
+Thread 1 Completed
+Thread 2 started
+Thread 2 completed
+
+Why this happen
+Every thread created copy of variable in cpu cache which is separate for each thread.
+This value is updated on each thread cache
+So for second thread isFlag true condition, and it is continuously running.
+
+There are two methods to avoid it
+1) Volatile keyword
+   when you use volatile keyword there is no copy of variable is made in respective CPU. Thread will access the value of variable directly.
+   Volatile is ony used for member variables.
+   Performance is relatively high compare to synchronized keyword.
+   volatile keyword helps us with visibility as memory value is access by thread not local cache value.
+   for increment and decrement use atomic keyword.
+
+2) Synchronized keyword
+   When synchronized keyword is used then until method complete the execution, no other method will be able to access the variable
+   This is only used for method and block
+   In this locking and unlocking take place so can cause deadlock
+
+* */
+
+
