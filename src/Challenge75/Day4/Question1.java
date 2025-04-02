329cee6fe1a22ca0e6144208e73d20f6f42354e1
RAHUL <rahulmulik9@gmail.com>
2025-04-02 19:33:57 +0530

    Day 4 Question1 152. Maximum Product Subarray : My approach

---
+++ src/Challenge75/Day4/Question1.java
@@ -0,0 +1,22 @@
+package Challenge75.Day4;
+/*152. Maximum Product Subarray*/
+public class Question1 {
+    static int maxProductSubArray(int arr[]) {
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = i + 1; j < arr.length; j++) {
+                int prod = 1;
+                for (int k = i; k <= j; k++)
+                    prod *= arr[k];
+                result = Math.max(result, prod);
+            }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int nums[] = {1, 2, -3, 0, -4, -5};
+        int answer = maxProductSubArray(nums);
+        System.out.print("The maximum product subarray is: " + answer);
+    }
+}
