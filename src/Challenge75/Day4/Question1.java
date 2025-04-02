7ba92ccf60816fccb655ab59c94033044bd0d2e2
RAHUL <rahulmulik9@gmail.com>
2025-04-02 19:37:22 +0530

    Day 4 Question1 152. Maximum Product Subarray :Better approach

--- src/Challenge75/Day4/Question1.java
+++ src/Challenge75/Day4/Question1.java
@@ -35,7 +35,7 @@ public class Question1 {
 
     public static void main(String[] args) {
         int nums[] = {1, 2, -3, 0, -4, -5};
-        int answer = maxProductSubArray(nums);
+        int answer = maxProductSubArray1(nums);
         System.out.print("The maximum product subarray is: " + answer);
     }
 }
