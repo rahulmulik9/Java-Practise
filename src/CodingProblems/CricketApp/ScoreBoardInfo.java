935d91f0a71992dd0ba69785e071e61906a84eca
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2025-12-02 18:24:02 +0530

    Player info added

--- src/CodingProblems/CricketApp/ScoreBoardInfo.java
+++ src/CodingProblems/CricketApp/ScoreBoardInfo.java
@@ -1,4 +1,42 @@
 package CodingProblems.CricketApp;
 
 public class ScoreBoardInfo {
+    private String name;
+    private int score;
+    private int ballFaced;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getBallFaced() {
+        return ballFaced;
+    }
+
+    public void setBallFaced(int ballFaced) {
+        this.ballFaced = ballFaced;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    private int index;
+
 }
