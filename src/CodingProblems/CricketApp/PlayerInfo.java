309083c4e715db51a3701144a2d75ea7b3adec6f
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-04 10:36:14 +0530

    Added player class method and required varibles

--- src/CodingProblems/CricketApp/PlayerInfo.java
+++ src/CodingProblems/CricketApp/PlayerInfo.java
@@ -1,4 +1,59 @@
 package CodingProblems.CricketApp;
 
 public class PlayerInfo {
+    private String name;
+    private int score;
+    private int ballFaced;
+    private int fourCount;
+    private int sixCount;
+    private int index;
+    private boolean isOut;
+
+    public boolean isOut() {
+        return isOut;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getBallFaced() {
+        return ballFaced;
+    }
+
+    public int getFourCount() {
+        return fourCount;
+    }
+
+    public int getSixCount() {
+        return sixCount;
+    }
+
+    public void addBall() {
+        ballFaced = ballFaced++;
+    }
+
+    public void addRuns(int runs, boolean isFour, boolean isSix) {
+        score = score + runs;
+        if (isFour) fourCount = fourCount++;
+        if (isSix) fourCount = sixCount++;
+    }
+
+    public void setOut(boolean out) {
+        isOut = out;
+    }
+
+    public PlayerInfo(int index, String name, boolean isOut) {
+        this.index = index;
+        this.name = name;
+        this.isOut = isOut;
+    }
 }
