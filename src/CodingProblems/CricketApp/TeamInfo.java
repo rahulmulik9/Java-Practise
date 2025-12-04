12972da86c2a3dfbdca9a44e37bc86a106f1a96e
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-04 10:51:17 +0530

    Added team class method and required varibles

--- src/CodingProblems/CricketApp/TeamInfo.java
+++ src/CodingProblems/CricketApp/TeamInfo.java
@@ -1,4 +1,59 @@
 package CodingProblems.CricketApp;
 
+import java.util.ArrayList;
+
 public class TeamInfo {
+    private int playerCount;
+    private int score;
+    private int nextPlayerIndex;
+    private int strikerIndex;
+    private int nonStrikerIndex;
+    private boolean isAllOut;
+    private ArrayList<PlayerInfo> playerList;
+
+    public TeamInfo(boolean isAllOut, int playerCount) {
+        this.isAllOut = isAllOut;
+        this.playerCount = playerCount;
+
+        for (int i = 0; i < playerCount; i++) {
+            PlayerInfo newPlayer = new PlayerInfo(i, ("Player"+1),false);
+            assert false;
+            playerList.add(newPlayer);
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getNextPlayerIndex() {
+        return nextPlayerIndex;
+    }
+
+    public int getStrikerIndex() {
+        return strikerIndex;
+    }
+
+    public int getNonStrikerIndex() {
+        return nonStrikerIndex;
+    }
+
+    public boolean isAllOut() {
+        return isAllOut;
+    }
+
+    public void addRuns(int runs) {
+        score = score + runs;
+    }
+
+    public void wicket() {
+        nextPlayerIndex = nextPlayerIndex++;
+        if (nextPlayerIndex > playerCount) isAllOut = true;
+    }
+
+    public void changeStricke() {
+        int temp = strikerIndex;
+        strikerIndex = nonStrikerIndex;
+        nonStrikerIndex = temp;
+    }
 }
