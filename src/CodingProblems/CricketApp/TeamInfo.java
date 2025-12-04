aa77f3a83956bad573721c7685c34c9aaf63ab72
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-04 13:23:16 +0530

    Reframe player class

--- src/CodingProblems/CricketApp/TeamInfo.java
+++ src/CodingProblems/CricketApp/TeamInfo.java
@@ -3,81 +3,22 @@ package CodingProblems.CricketApp;
 import java.util.ArrayList;
 
 public class TeamInfo {
-    private int playerCount;
+    private ArrayList<PlayerInfo> playerList;
+    private int totalPlayer;
     private int score;
+    private int extraScore;
+    private int numberOfWicketFallen;
     private int nextPlayerIndex;
     private int strikerIndex;
     private int nonStrikerIndex;
-    private boolean isAllOut;
-    private ArrayList<PlayerInfo> playerList = new ArrayList<>();
-    private int extraScore;
-
-    private int lastWicketFall;
-
     public TeamInfo(boolean isAllOut, int playerCount) {
-        this.isAllOut = isAllOut;
-        this.playerCount = playerCount;
+        playerList = new ArrayList<>();
+        totalPlayer = playerCount;
         nextPlayerIndex = 2;
+        numberOfWicketFallen = 0;
         for (int i = 0; i < playerCount; i++) {
             PlayerInfo newPlayer = new PlayerInfo(i, ("Player"+i),false);
             playerList.add(newPlayer);
         }
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public int getNextPlayerIndex() {
-        return nextPlayerIndex;
-    }
-
-    public int getStrikerIndex() {
-        return strikerIndex;
-    }
-
-    public int getNonStrikerIndex() {
-        return nonStrikerIndex;
-    }
-
-    public boolean isAllOut() {
-        return isAllOut;
-    }
-
-    public void addRuns(int runs, boolean isFour, boolean isSix) {
-        score = score + runs;
-        if (runs==4)isFour = true;
-        if (runs==6)isSix = true;
-        playerList.get(strikerIndex).addRuns(runs,isFour,isSix);
-    }
-
-    public void addBall(){
-        playerList.get(strikerIndex).addBall();
-
-    }
-    public void wicket() {
-        lastWicketFall = strikerIndex;
-
-        strikerIndex = nextPlayerIndex;
-        nextPlayerIndex++;
-        if (nextPlayerIndex > playerCount) isAllOut = true;
-    }
-
-    public void changeStricke() {
-        int temp = strikerIndex;
-        strikerIndex = nonStrikerIndex;
-        nonStrikerIndex = temp;
-    }
-
-    public void addExtra(int run){
-        score = score + run;
-        extraScore = extraScore+run;
-    }
-
-    public PlayerInfo getFallenWicketInfo(){
-        return playerList.get(lastWicketFall);
-    }
-    public PlayerInfo getNextPlayerInfo(){
-        return playerList.get(strikerIndex);
     }
 }
