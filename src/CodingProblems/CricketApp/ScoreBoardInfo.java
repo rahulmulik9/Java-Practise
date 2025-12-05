44d75b35ca55ad519f8d584629281b7840fd09c1
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-05 16:59:38 +0530

    Added ScoreBoardInfo

--- src/CodingProblems/CricketApp/ScoreBoardInfo.java
+++ src/CodingProblems/CricketApp/ScoreBoardInfo.java
@@ -1,4 +1,26 @@
 package CodingProblems.CricketApp;
 
 public class ScoreBoardInfo {
+    private TeamInfo firstTeam;
+    private TeamInfo secondTeam;
+    private int leagalBall;
+    private int overs;
+    private int totalPlayers;
+
+    public ScoreBoardInfo(int overs, int totalPlayers) {
+        firstTeam = new TeamInfo(totalPlayers);
+        secondTeam = new TeamInfo(totalPlayers);
+        leagalBall = 0;
+        this.overs = overs;
+        this.totalPlayers = totalPlayers;
+    }
+
+    public TeamInfo getFirstTeam() {
+        return firstTeam;
+    }
+
+    public TeamInfo getSecondTeam() {
+        return secondTeam;
+    }
+
 }
