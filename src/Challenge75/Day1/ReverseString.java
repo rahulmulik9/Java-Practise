0a01624c538f31ed807f979067a7b058feb357dc
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-15 19:29:31 +0530

    Reverse string using stack

--- src/Challenge75/Day1/ReverseString.java
+++ src/Challenge75/Day1/ReverseString.java
@@ -1,19 +1,18 @@
 package Challenge75.Day1;
 
+import java.util.Stack;
+
 public class ReverseString {
     public static void main(String[] args) {
         System.out.printf(ReverseTheString("HELLOWORLD"));
+        System.out.printf(reverseStringStack("HELLOWORLD"));
+        System.out.printf(reverseString("HELLOWORLD"));
     }
 
     private static String ReverseTheString(String s) {
         StringBuilder stringBuilder  = new StringBuilder(s);
         int left = 0, right = s.length() - 1;
-
-
-
-
         StringBuilder res = new StringBuilder(s);
-
         while (left < right) {
             char temp = res.charAt(left);
             res.setCharAt(left, res.charAt(right));
@@ -52,10 +51,29 @@ public class ReverseString {
         reverseStringRec(s, l + 1, r - 1);
     }
 
-
     static String reverseString(String s) {
         char[] arr = s.toCharArray();
         reverseStringRec(arr, 0, arr.length - 1);
         return new String(arr);
     }
+
+
+
+    ////using stack
+    static String reverseStringStack(String s) {
+        Stack<Character> st = new Stack<>();
+
+        // Push the characters into stack
+        for (int i = 0; i < s.length(); i++)
+            st.push(s.charAt(i));
+
+        StringBuilder res = new StringBuilder();
+
+        // Pop the characters of stack into the original string
+        for (int i = 0; i < s.length(); i++)
+            res.append(st.pop());
+
+        return res.toString();
+    }
+
 }
