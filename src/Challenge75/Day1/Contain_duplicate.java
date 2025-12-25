da0c22aae5edc6dff0d6b70f0400cc04258feb2c
Rahul Mulik <rahulmulik9@gmail.com>
2025-12-25 22:49:16 +0530

    Leetcode 217 Contain duplicates Approach using set and for loop to check

--- src/Challenge75/Day1/Contain_duplicate.java
+++ src/Challenge75/Day1/Contain_duplicate.java
@@ -1,4 +1,31 @@
 package Challenge75.Day1;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Contain_duplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,8,6,7,8,4,5,7,9,4,5};
+        int[] nums_1 = {1,8,6,7};
+
+        if(findDuplicate(nums)){
+            System.out.println("Duplicate Present");
+        }else {
+            System.out.println("Duplicate not found");
+        }
+    }
+
+    private static boolean findDuplicate(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>();
+
+        //approach first
+        for (int number:nums){
+            if(numsSet.contains(number)){
+                return true;
+            }else {
+                numsSet.add(number);
+            }
+        }
+        return false;
+    }
 }
