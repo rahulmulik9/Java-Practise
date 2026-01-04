1ddfb660e2a9c09eff97ef53c6cd0cb63da3867a
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-04 22:27:53 +0530

    Day 5 merge sorted linked list problem

--- src/Challenge75/Day5/Merge_Sorted_linkedList.java
+++ src/Challenge75/Day5/Merge_Sorted_linkedList.java
@@ -1,13 +1,15 @@
 package Challenge75.Day5;
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+ * Definition for singly-linked list. */
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+
 public class Merge_Sorted_linkedList {
 }
+
