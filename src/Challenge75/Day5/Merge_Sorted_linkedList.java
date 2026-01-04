38e0bff8bab7b15bf581cf8db05197eb86499ec6
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-04 22:30:06 +0530

    Day 5 merge sorted linked list problem Test created

--- src/Challenge75/Day5/Merge_Sorted_linkedList.java
+++ src/Challenge75/Day5/Merge_Sorted_linkedList.java
@@ -10,6 +10,40 @@ class ListNode {
 }
 
 
+
 public class Merge_Sorted_linkedList {
+    // Utility method to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // List 1: 1 -> 3 -> 5
+        ListNode l1 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(5)));
+
+        // List 2: 2 -> 4 -> 6
+        ListNode l2 = new ListNode(2,
+                new ListNode(4,
+                        new ListNode(6)));
+
+        System.out.print("List 1: ");
+        printList(l1);
+
+        System.out.print("List 2: ");
+        printList(l2);
+
+        // Merge lists
+        ListNode merged = mergeTwoLists(l1, l2);
+
+        System.out.print("Merged List: ");
+        printList(merged);
+    }
 }
 
