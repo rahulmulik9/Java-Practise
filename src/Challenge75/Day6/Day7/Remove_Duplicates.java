82705e7590c59510ea834df3d1a8293cc614c1da
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-11 17:34:52 +0530

    Solutions has been added

--- src/Challenge75/Day6/Day7/Remove_Duplicates.java
+++ src/Challenge75/Day6/Day7/Remove_Duplicates.java
@@ -1,4 +1,25 @@
 package Challenge75.Day6.Day7;
 
 public class Remove_Duplicates {
+    public static void main(String[] args) {
+        int[]nums = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(new SolutionDuplicate().removeDuplicates(nums));
+    }
 }
+
+class SolutionDuplicate {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
+
