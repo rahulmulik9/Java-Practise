4e98936887cd40397693912e161808f80f3fe8db
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-12 10:41:47 +0530

    Question IF it is static then initialise it in static block

--- src/basic/AccessModifiers/FinalVariables.java
+++ src/basic/AccessModifiers/FinalVariables.java
@@ -1,6 +1,24 @@
 package basic.AccessModifiers;
 
 //can you declare variable as final and initialise it later
+//Ans : Yes
+//IF it is static then initialise it in static block
+//If it is member variable then declared in constructor.
+//If it is local then initialise it in method itself
+
 
 public class FinalVariables {
+    public static void main(String[] args) {
+
+    }
+}
+
+class FinalVaribles{
+    //IF it is static then initialise it in static block
+    public static final int MAX_VALUE;
+    static {
+        MAX_VALUE = 100;
+    }
+
+
 }
