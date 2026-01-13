f7db249c4c381870028a1f6396a1a01f0f867403
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-13 21:43:10 +0530

    First occurrence of string

--- src/Challenge75/Day8/First_Occurance_String.java
+++ src/Challenge75/Day8/First_Occurance_String.java
@@ -3,3 +3,15 @@ package Challenge75.Day8;
 public class First_Occurance_String {
 }
 
+class SolutionFirst {
+    public int strStr(String haystack, String needle) {
+        final int m = haystack.length();
+        final int n = needle.length();
+
+        for (int i = 0; i < m - n + 1; ++i)
+            if (haystack.substring(i, i + n).equals(needle))
+                return i;
+
+        return -1;
+    }
+}
