fb463f113c6a1ec78048bcadf3d89f708946c77d
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-15 22:26:07 +0530

    Plus one problem statement

--- src/Challenge75/Day10/Plus_One.java
+++ src/Challenge75/Day10/Plus_One.java
@@ -1,10 +1,16 @@
 package Challenge75.Day10;
 
+import java.util.Arrays;
+
 /*
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
 The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 Increment the large integer by one and return the resulting array of digits.*/
 public class Plus_One {
+    public static void main(String[] args) {
+        int[] nums={1,2,3};
+        Arrays.stream(new SolutionPlusOne().plusOne(nums)).forEach(System.out::print);
+    }
 }
 
 class SolutionPlusOne {
