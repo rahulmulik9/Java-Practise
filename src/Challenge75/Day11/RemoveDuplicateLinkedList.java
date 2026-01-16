067eaff2be3f3bda58bd5844ce7e00285765588f
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-16 22:04:25 +0530

    Added linked list node

--- src/Challenge75/Day11/RemoveDuplicateLinkedList.java
+++ src/Challenge75/Day11/RemoveDuplicateLinkedList.java
@@ -1,4 +1,17 @@
 package Challenge75.Day11;
 
+
 public class RemoveDuplicateLinkedList {
 }
+
+/**
+ * Definition for singly-linked list. */
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+
+
