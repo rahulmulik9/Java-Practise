41975f07565b9280aadf75f2a0e47b3f3ad923b6
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-16 22:04:43 +0530

    Added algorithm

--- src/Challenge75/Day11/RemoveDuplicateLinkedList.java
+++ src/Challenge75/Day11/RemoveDuplicateLinkedList.java
@@ -14,4 +14,16 @@ class ListNode {
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  }
 
+class SolutionRemovedLinkesList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
 
+        while (curr != null) {
+            while (curr.next != null && curr.val == curr.next.val)
+                curr.next = curr.next.next;
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
