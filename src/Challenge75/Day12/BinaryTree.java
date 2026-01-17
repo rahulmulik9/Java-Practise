4609b2eac13b353fef75081f22390660d47c28a4
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-17 23:09:36 +0530

    Added tree node

---
+++ src/Challenge75/Day12/BinaryTree.java
@@ -0,0 +1,14 @@
+package Challenge75.Day12;
+
+public class BinaryTree {
+}
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int value) {
+        data = value;
+        left = right = null;
+    }
+}
