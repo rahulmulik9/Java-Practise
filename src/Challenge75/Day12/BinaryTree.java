5517915a35211fe5ee3d125e76c1f295b93c3067
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-17 23:10:30 +0530

    Pre-Order tree traversal

--- src/Challenge75/Day12/BinaryTree.java
+++ src/Challenge75/Day12/BinaryTree.java
@@ -12,3 +12,14 @@ class Node {
         left = right = null;
     }
 }
+class BinaryTreeExample {
+    // Preorder: Root -> Left -> Right
+    static void preorder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+}
