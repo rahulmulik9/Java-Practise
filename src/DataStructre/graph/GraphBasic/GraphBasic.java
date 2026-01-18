effc0c529065557565e6f9e90d28abb50a372fd3
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-18 21:19:54 +0530

    Filled Graph edge

--- src/DataStructre/graph/GraphBasic/GraphBasic.java
+++ src/DataStructre/graph/GraphBasic/GraphBasic.java
@@ -15,11 +15,21 @@ class Graph {
             adjacencyList.add(new ArrayList<>());
         }
     }
+
+    public void addEdge(int source, int destination) {
+        adjacencyList.get(source).add(destination);
+        adjacencyList.get(destination).add(source);
+    }
 }
 
 public class GraphBasic {
     public static void main(String[] args) {
+        Graph graph = new Graph(4);
 
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
     }
 }
 
