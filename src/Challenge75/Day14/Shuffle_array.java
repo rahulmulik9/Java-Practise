2dc4f0a38f3c433199f60683be0953f5cda1c43d
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-19 21:24:17 +0530

    Added Solutions

--- src/Challenge75/Day14/Shuffle_array.java
+++ src/Challenge75/Day14/Shuffle_array.java
@@ -5,6 +5,20 @@ public class Shuffle_array {
 
     }
 }
-class Solution {
+class SolutionShufle {
+    public int[] shuffle(int[] nums, int n) {
+        // Create result array with size 2n (using bit shift for multiplication by 2)
+        int[] result = new int[n << 1];
 
+        // Iterate through first half of the array
+        for (int i = 0, resultIndex = 0; i < n; i++) {
+            // Add element from first half (index i)
+            result[resultIndex++] = nums[i];
+
+            // Add corresponding element from second half (index i + n)
+            result[resultIndex++] = nums[i + n];
+        }
+
+        return result;
+    }
 }
