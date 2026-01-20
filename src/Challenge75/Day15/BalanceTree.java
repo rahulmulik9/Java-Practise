a697f78b58d6c9fe2684414c357b6f37629fc91f
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-20 21:53:47 +0530

    Tree Node

--- src/Challenge75/Day15/BalanceTree.java
+++ src/Challenge75/Day15/BalanceTree.java
@@ -1,4 +1,16 @@
 package Challenge75.Day15;
+class TreeNode {
+    int data;
+    Node left;
+    Node right;
+
+    TreeNode(int d) {
+        int data = d;
+        this.left = null;
+        this.right = null;
+    }
+}
 
 public class BalanceTree {
+
 }
