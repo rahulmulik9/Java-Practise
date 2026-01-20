5c5a7723ff8a14fd08edf47461b88bfc7e82178b
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-20 21:54:26 +0530

    Balance Tree main

--- src/Challenge75/Day15/BalanceTree.java
+++ src/Challenge75/Day15/BalanceTree.java
@@ -1,16 +1,18 @@
 package Challenge75.Day15;
-class TreeNode {
-    int data;
-    Node left;
-    Node right;
-
-    TreeNode(int d) {
-        int data = d;
-        this.left = null;
-        this.right = null;
-    }
-}
-
 public class BalanceTree {
+    public static void main(String[] args) {
+        // Representation of input BST:
+        //            10
+        //           / \
+        //          20   30
+        //         /  \
+        //        40   60
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(60);
 
+        System.out.println(isBalanced(root) ? "true" : "false");
+    }
 }
