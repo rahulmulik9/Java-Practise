807b4dd958d285438066b8e05d2239fe8e3b43f0
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-20 21:49:31 +0530

    Node added

--- src/Challenge75/Day15/HeightTree.java
+++ src/Challenge75/Day15/HeightTree.java
@@ -1,4 +1,13 @@
 package Challenge75.Day15;
+class Node {
+    int data;
+    Node left, right;
 
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
 public class HeightTree {
 }
