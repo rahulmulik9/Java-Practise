d23fdcf89bde7d85ee1416f8fbeda7dc775cde62
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-21 21:12:18 +0530

    Added Tree Node

--- src/Challenge75/Day16/BalancedBinaryTree.java
+++ src/Challenge75/Day16/BalancedBinaryTree.java
@@ -1,4 +1,14 @@
 package Challenge75.Day16;
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
 
 public class BalancedBinaryTree {
 }
