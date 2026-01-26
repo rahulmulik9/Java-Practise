98b1c94cce0fc6c6b941fac593ba6ca9b42d59a6
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-26 16:12:30 +0530

    Added problem statement

--- src/Challenge75/Day18/SingleNumber.java
+++ src/Challenge75/Day18/SingleNumber.java
@@ -1,4 +1,8 @@
 package Challenge75.Day18;
 
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.*/
+
 public class SingleNumber {
 }
+
