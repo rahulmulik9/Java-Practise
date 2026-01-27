48f9f267b5370db7d509ab42244b5e3d127faf14
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-27 22:56:44 +0530

    Added solutions

--- src/Challenge75/Day19/ValidPar.java
+++ src/Challenge75/Day19/ValidPar.java
@@ -1,5 +1,27 @@
 package Challenge75.Day19;
 
 public class ValidPar {
+    public static void main(String[] args) {
 
+    }
+}
+
+class SolutionValidPar {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+                ++l;
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+                --r;
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                return false;
+            ++l;
+            --r;
+        }
+
+        return true;
+    }
 }
