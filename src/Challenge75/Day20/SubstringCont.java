619b1a04c09f566a630581255a6476ad31396dd7
Rahul Mulik <rahulmulik9@gmail.com>
2026-01-28 20:50:32 +0530

    Problem statement

--- src/Challenge75/Day20/SubstringCont.java
+++ src/Challenge75/Day20/SubstringCont.java
@@ -1,5 +1,12 @@
 package Challenge75.Day20;
+/*You are given a string s and an array of strings words. All the strings of words are of the same length.
 
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+ */
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
