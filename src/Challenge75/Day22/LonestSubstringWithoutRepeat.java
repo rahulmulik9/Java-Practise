9a007ab4aeb91eabb90ff9c168a580f62081087a
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-02 20:12:13 +0530

    Added main method

--- src/Challenge75/Day22/LonestSubstringWithoutRepeat.java
+++ src/Challenge75/Day22/LonestSubstringWithoutRepeat.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class LonestSubstringWithoutRepeat {
-
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(new SolutionLongestSubstring().lengthOfLongestSubstring(s));
+    }
 }
 
