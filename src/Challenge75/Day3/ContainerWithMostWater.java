aedc14a2f08dade76d9a5430b40e21aa0255df5d
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-03 15:17:09 +0530

    Added solutions

--- src/Challenge75/Day3/ContainerWithMostWater.java
+++ src/Challenge75/Day3/ContainerWithMostWater.java
@@ -3,4 +3,31 @@ package Challenge75.Day3;
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 Return the maximum amount of water a container can store.*/
 public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+    }
+}
+
+class SolutionContain{
+    public static int maxArea(int [] heights){
+        int left = 0;
+        int right = heights.length-1;
+        int maxArea = 0;
+
+        while(left<right){
+            int h = Math.min(heights[left],heights[right]);
+            int w = right-left;
+            int area = h*w;
+            maxArea =Math.max(maxArea,area);
+
+            if(heights[left]>heights[right]){
+                right--;
+            }else {
+                left++;
+            }
+
+        }
+        return maxArea;
+    }
+
 }
