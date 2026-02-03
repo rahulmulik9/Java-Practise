1c28b9d8d0b7840ccf1f601357940d25642279fb
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-03 15:57:43 +0530

    Stream example

--- src/Challenge75/Day3/ContainerWithMostWater.java
+++ src/Challenge75/Day3/ContainerWithMostWater.java
@@ -1,4 +1,8 @@
 package Challenge75.Day3;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /*You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 Return the maximum amount of water a container can store.*/
@@ -6,6 +10,7 @@ public class ContainerWithMostWater {
     public static void main(String[] args) {
         int[] height = {1,8,6,2,5,4,8,3,7};
         System.out.println(new SolutionContain().maxArea(height));
+        new SolutionContain().wordsCharArr();
     }
 }
 
@@ -31,4 +36,15 @@ class SolutionContain{
         return maxArea;
     }
 
+    public void wordsCharArr(){
+
+        List<String> stringList = List.of("INDIA","COUNTRY");
+
+        List<Character> charList = stringList.stream().
+                flatMap(word->word.chars().mapToObj(c->(char)c)).toList();
+        stringList.stream()
+                .flatMap(word -> word.chars().mapToObj(c -> (char) c))
+                .forEach(System.out::print);
+
+    }
 }
