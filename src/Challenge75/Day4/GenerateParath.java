1286fd9677f55733771d85ad96a9c74b4643b343
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-04 09:19:05 +0530

    Added question

--- src/Challenge75/Day4/GenerateParath.java
+++ src/Challenge75/Day4/GenerateParath.java
@@ -1,4 +1,12 @@
 package Challenge75.Day4;
 
+/*Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]*/
 public class GenerateParath {
+    public static void main(String[] args) {
+
+    }
 }
+
