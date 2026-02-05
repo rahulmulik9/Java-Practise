bae39a93c711a4849741e1575d492fef7b39132d
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-05 09:32:45 +0530

    Added solutions

--- src/Challenge75/Day5/ContainDuplicate.java
+++ src/Challenge75/Day5/ContainDuplicate.java
@@ -1,16 +1,26 @@
 package Challenge75.Day5;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*Print true if array contains duplicates*/
 public class ContainDuplicate {
     public static void main(String[] args) {
-        int[] nums = {1,5,9,6,3,2,4,7};
-        System.out.println(new SolutionContainDuplicate().IsDuplicatePresnet(nums));
+        int[] nums = {1,5,2,9,6,3,2,4,7};
+        System.out.println(new SolutionContainDuplicate().IsDuplicatePresent(nums));
     }
 }
 
 class SolutionContainDuplicate{
     public boolean IsDuplicatePresent(int[] nums){
-
+        Set<Integer> checkset = new HashSet<>();
+        for (int num : nums){
+            if(checkset.contains(num)){
+                return true;
+            }
+            checkset.add(num);
+        }
+        return false;
     }
 
 }
