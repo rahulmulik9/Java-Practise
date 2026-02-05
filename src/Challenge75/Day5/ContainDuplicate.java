e6209cb826469b852537f283f719d5a0d7d72bde
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-05 09:30:56 +0530

    Class added

--- src/Challenge75/Day5/ContainDuplicate.java
+++ src/Challenge75/Day5/ContainDuplicate.java
@@ -1,4 +1,18 @@
 package Challenge75.Day5;
 
+/*Print true if array contains duplicates*/
 public class ContainDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,5,9,6,3,2,4,7};
+        System.out.println(new SolutionContainDuplicate().IsDuplicatePresnet(nums));
+    }
 }
+
+class SolutionContainDuplicate{
+    public boolean IsDuplicatePresent(int[] nums){
+
+    }
+
+}
+
+
