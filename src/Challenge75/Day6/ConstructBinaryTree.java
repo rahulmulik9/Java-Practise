3eb467c46fc9f824d06a24de40bb4066a9716a07
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-06 07:35:42 +0530

    Added tree node

--- src/Challenge75/Day6/ConstructBinaryTree.java
+++ src/Challenge75/Day6/ConstructBinaryTree.java
@@ -1,4 +1,18 @@
 package Challenge75.Day6;
 /*Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.*/
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class ConstructBinaryTree {
 }
+
