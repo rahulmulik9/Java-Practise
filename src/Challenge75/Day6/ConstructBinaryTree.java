585a47e07f75a5c1d506b6b6d18b98ae4f57a18f
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-06 07:34:52 +0530

    Added problem statement

--- src/Challenge75/Day6/ConstructBinaryTree.java
+++ src/Challenge75/Day6/ConstructBinaryTree.java
@@ -1,4 +1,4 @@
 package Challenge75.Day6;
-
+/*Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.*/
 public class ConstructBinaryTree {
 }
