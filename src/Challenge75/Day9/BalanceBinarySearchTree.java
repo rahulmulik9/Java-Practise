5d40b9c7f928aac2cc958946e8b8585dbe111012
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-09 10:38:58 +0530

    Tree node added

--- src/Challenge75/Day9/BalanceBinarySearchTree.java
+++ src/Challenge75/Day9/BalanceBinarySearchTree.java
@@ -1,6 +1,20 @@
 package Challenge75.Day9;
 /*Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
 A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.*/
+
+ class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+     }
+ }
+
 public class BalanceBinarySearchTree {
 
 }
