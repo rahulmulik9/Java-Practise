ae794a1752b574a71ea4cd4913b20c2d65c8a132
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-18 17:21:21 +0530

    Added linked list

--- src/Challenge75/Day18/CycleLinkedList.java
+++ src/Challenge75/Day18/CycleLinkedList.java
@@ -1,4 +1,13 @@
 package Challenge75.Day18;
 
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+         next = null;
+     }
+}
+
 public class CycleLinkedList {
 }
