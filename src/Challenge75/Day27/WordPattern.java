7b598b855d5484fea2013d97e46156c9177bbf7c
Rahul Mulik <rahulmulik9@gmail.com>
2026-02-27 10:15:47 +0530

    Optimal solutions added

--- src/Challenge75/Day27/WordPattern.java
+++ src/Challenge75/Day27/WordPattern.java
@@ -23,6 +23,7 @@ Output: false
 */
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class WordPattern {
     public static void main(String[] args) {
@@ -35,23 +36,20 @@ public class WordPattern {
 
 class PatterSolutions {
     boolean checkPatternExistOrNot(String pattern, String s) {
-
-        char[] patternAty = pattern.toCharArray();
-        String[] stryAry = s.split(" ");
-
-        if (patternAty.length != stryAry.length) return false;
-
-        HashMap<Character, String> storeMpa = new HashMap<>();
-        for (int i = 0; i < patternAty.length; i++) {
-            if (storeMpa.containsKey(patternAty[i])) {
-                String values = storeMpa.get(patternAty[i]);
-                String requiredvaue = stryAry[i];
-                if (!values.equals(requiredvaue)) {
-                    return false;
-                }
-            }else{
-             storeMpa.put(patternAty[i],stryAry[i]) ;
+        String[] ws = s.split(" ");
+        if (pattern.length() != ws.length) {
+            return false;
+        }
+        Map<Character, String> d1 = new HashMap<>();
+        Map<String, Character> d2 = new HashMap<>();
+        for (int i = 0; i < ws.length; ++i) {
+            char a = pattern.charAt(i);
+            String b = ws[i];
+            if (!d1.getOrDefault(a, b).equals(b) || d2.getOrDefault(b, a) != a) {
+                return false;
             }
+            d1.put(a, b);
+            d2.put(b, a);
         }
         return true;
     }
