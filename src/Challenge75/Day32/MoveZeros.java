1157a41c4a3ab962a00bb40ed15280143e2e7fa3
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-04 17:17:12 +0530

    Solutions added

--- src/Challenge75/Day32/MoveZeros.java
+++ src/Challenge75/Day32/MoveZeros.java
@@ -5,7 +5,13 @@ import java.util.Arrays;
 public class MoveZeros {
     public static void main(String[] args) {
         int[] intary = {0, 5, 6, 0, 4, 8, 0, 6, 7};
+        int[] intary1= {0};
+        int[] intary2= {1,3,5,6};
         Arrays.stream(new MoveZeroSol().MoveZeros(intary)).forEach(System.out::print);
+        System.out.println("");
+        Arrays.stream(new MoveZeroSol().MoveZeros(intary1)).forEach(System.out::print);
+        System.out.println("");
+        Arrays.stream(new MoveZeroSol().MoveZeros(intary2)).forEach(System.out::print);
     }
 }
 
