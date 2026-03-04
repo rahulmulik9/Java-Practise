2dc038d935067cef7574b933ae9c9d436e48eeaf
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-04 17:11:44 +0530

    Stream call

--- src/Challenge75/Day32/MoveZeros.java
+++ src/Challenge75/Day32/MoveZeros.java
@@ -1,6 +1,12 @@
 package Challenge75.Day32;
 
+import java.util.Arrays;
+
 public class MoveZeros {
+    public static void main(String[] args) {
+        int[] intary= {0,5,6,0,4,8,0,6,7};
+        Arrays.stream(new MoveZeroSol().MoveZeros(intary)).forEach(System.out::println);
+    }
 }
 
 class MoveZeroSol{
