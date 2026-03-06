3b8225caf7a9cb6820560aad45e002a60b4b60a0
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-06 22:26:07 +0530

    Solution added

--- src/Challenge75/Day34/ContiniousSegment.java
+++ src/Challenge75/Day34/ContiniousSegment.java
@@ -1,7 +1,32 @@
 package Challenge75.Day34;
 
+/*PROBLEM STATEMENT
+* Given a binary string s without leading zeros, return true if s contains at most one contiguous segment of ones. Otherwise, return false.
+Example 1:
+
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+Example 2:
+
+Input: s = "110"
+Output: true*/
 public class ContiniousSegment {
     public static void main(String[] args) {
+        String first = "1001";
+        String second = "10110";
+        System.out.println(new ContinousSol().isContiniousOne(first));
+    }
+}
 
+class ContinousSol{
+    public boolean isContiniousOne(String s){
+        char[] arr = s.toCharArray();
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i]==arr[i-1]){
+                return true;
+            }
+        }
+        return false;
     }
 }
