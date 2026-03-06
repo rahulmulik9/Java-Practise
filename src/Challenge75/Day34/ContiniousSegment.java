ac84c8359a4fdcdf441bd2ecbc650da0ece2645f
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-06 22:41:40 +0530

    Final simple solutions

--- src/Challenge75/Day34/ContiniousSegment.java
+++ src/Challenge75/Day34/ContiniousSegment.java
@@ -15,24 +15,31 @@ public class ContiniousSegment {
     public static void main(String[] args) {
         String first = "1001";
         String second = "10110";
+        String third = "1";
         System.out.println(new ContinousSol().isContiniousOne(first));
         System.out.println(new ContinousSol().isContiniousOne(second));
+        System.out.println(new ContinousSol().isContiniousOne(third));
     }
 }
 
 class ContinousSol{
     public boolean isContiniousOne(String s){
-        char[] arr = s.toCharArray();
-
-        if(arr.length==0)return false;
-        if(arr.length==1 && arr[0]=='1'){
-            return true;
-        }
-        for (int i = 1; i <arr.length ; i++) {
-            if( arr[i]=='1' && arr[i]==arr[i-1] ){
-                return true;
-            }
-        }
-        return false;
+//        char[] arr = s.toCharArray();
+//
+//        if(arr.length==0)return false;
+//        if(arr.length==1 && arr[0]=='1'){
+//            return true;
+//        }
+//        if(arr.length==2){
+//            if (arr[0]=='1' || arr[1]=='1'){
+//            return true;}
+//        }
+//        for (int i = 1; i <arr.length ; i++) {
+//            if( arr[i]=='1' && arr[i]==arr[i-1] ){
+//                return true;
+//            }
+//        }
+//        return false;
+        return !s.contains("01");
     }
 }
