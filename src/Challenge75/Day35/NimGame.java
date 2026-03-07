2c9212dd0f44cfc7ea77b121c94b69fddb0c4ea1
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-07 23:46:08 +0530

    Soltions adeed

--- src/Challenge75/Day35/NimGame.java
+++ src/Challenge75/Day35/NimGame.java
@@ -1,4 +1,14 @@
 package Challenge75.Day35;
-
+/*class Solution {
+  public boolean canWinNim(int n) {
+    return n % 4 != 0;
+  }
+}*/
 public class NimGame {
+}
+
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
 }
