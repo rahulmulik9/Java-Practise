3834d91dd95e1598b24afec76760b6b4f6aed373
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-09 18:44:29 +0530

    Problem statement and class added

--- src/Challenge75/Day27/IsometricString.java
+++ src/Challenge75/Day27/IsometricString.java
@@ -1,4 +1,13 @@
 package Challenge75.Day27;
+/*PROBLEM STATEMENT
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+ */
 
 public class IsometricString {
+}
+
+class IsoMetricStringSol{
+
 }
