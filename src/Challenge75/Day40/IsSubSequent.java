f07bf7ed2c8e87874e3b19aa10f58e5ce6d697aa
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-12 20:42:55 +0530

    Solutins added

--- src/Challenge75/Day40/IsSubSequent.java
+++ src/Challenge75/Day40/IsSubSequent.java
@@ -3,3 +3,17 @@ package Challenge75.Day40;
 public class IsSubSequent {
 }
 
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.isEmpty())
+            return true;
+
+        int i = 0;
+        for (final char c : t.toCharArray())
+            if (s.charAt(i) == c && ++i == s.length())
+                return true;
+
+        return false;
+    }
+}
