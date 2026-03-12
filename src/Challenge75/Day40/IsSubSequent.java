930aed77f3af930dd037f2b4a9fe92c689686182
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-12 20:47:07 +0530

    PS

--- src/Challenge75/Day40/IsSubSequent.java
+++ src/Challenge75/Day40/IsSubSequent.java
@@ -1,5 +1,7 @@
 package Challenge75.Day40;
+/*Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).*/
 public class IsSubSequent {
     public static void main(String[] args) {
 
