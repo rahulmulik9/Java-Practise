6c27cef089ca6fe3b50922265e287475b7889843
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-13 19:04:54 +0530

    linked list added

--- src/Challenge75/Day41/PalindromeLinkedList.java
+++ src/Challenge75/Day41/PalindromeLinkedList.java
@@ -1,4 +1,12 @@
 package Challenge75.Day41;
 
+class ListNode {
+    int val;
+    ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 public class PalindromeLinkedList {
 }
+
