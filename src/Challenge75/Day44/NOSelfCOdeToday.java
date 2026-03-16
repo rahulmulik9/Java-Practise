78ceca6823c51a2808d48c40704df68dd074f9ac
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-16 18:19:08 +0530

    Added solutions

--- src/Challenge75/Day44/NOSelfCOdeToday.java
+++ src/Challenge75/Day44/NOSelfCOdeToday.java
@@ -1,4 +1,25 @@
 package Challenge75.Day44;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class NOSelfCOdeToday {
+}
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < numRows; ++i) {
+            Integer[] temp = new Integer[i + 1];
+            Arrays.fill(temp, 1);
+            ans.add(Arrays.asList(temp));
+        }
+
+        for (int i = 2; i < numRows; ++i)
+            for (int j = 1; j < ans.get(i).size() - 1; ++j)
+                ans.get(i).set(j, ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+
+        return ans;
+    }
 }
