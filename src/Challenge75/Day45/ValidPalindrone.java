3589a48cfbdca67f605f793651b493cc2c9fd93c
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-19 10:15:41 +0530

    Class added

--- src/Challenge75/Day45/ValidPalindrone.java
+++ src/Challenge75/Day45/ValidPalindrone.java
@@ -2,3 +2,22 @@ package Challenge75.Day45;
 
 public class ValidPalindrone {
 }
+class SolutionPali {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+                ++l;
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+                --r;
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                return false;
+            ++l;
+            --r;
+        }
+
+        return true;
+    }
+}
