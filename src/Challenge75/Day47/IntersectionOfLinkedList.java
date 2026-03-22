93992bbd8d31131d6e2c7f59cdd01dccea0742f9
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-22 15:42:57 +0530

    Added LinkedList

--- src/Challenge75/Day47/IntersectionOfLinkedList.java
+++ src/Challenge75/Day47/IntersectionOfLinkedList.java
@@ -1,4 +1,15 @@
 package Challenge75.Day47;
-
+class ListNode {
+    int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+}
 public class IntersectionOfLinkedList {
+    public static void main(String[] args) {
+
+    }
+
 }
