5378c12191125c5fa3c9e1c4f84cef8f0dee393d
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-24 17:03:11 +0530

    Added Tree Node and solutions

--- src/Challenge75/Day49/SymmetricTree.java
+++ src/Challenge75/Day49/SymmetricTree.java
@@ -1,4 +1,29 @@
 package Challenge75.Day49;
-
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+}
 public class SymmetricTree {
+
+}
+
+class SolutionSyT {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root, root);
+    }
+
+    private boolean isSymmetric(TreeNode p, TreeNode q) {
+        if (p == null || q == null)
+            return p == q;
+
+        return p.val == q.val && isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+    }
 }
