e2a0ebcdfc4ea4cd6e259b840e1fa14ef29a499a
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-25 15:26:31 +0530

    Added solutions

--- src/Challenge75/Day50/MaxDepthOfBinaryTree.java
+++ src/Challenge75/Day50/MaxDepthOfBinaryTree.java
@@ -1,4 +1,22 @@
 package Challenge75.Day50;
-
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class MaxDepthOfBinaryTree {
+}
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
 }
