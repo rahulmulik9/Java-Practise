c9cb579e0e0290b910f1c435c12ebbd780bca77a
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-25 15:27:38 +0530

    Added test case

--- src/Challenge75/Day50/MaxDepthOfBinaryTree.java
+++ src/Challenge75/Day50/MaxDepthOfBinaryTree.java
@@ -12,6 +12,17 @@ class TreeNode {
     }
 }
 public class MaxDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+
+        Solution sol = new Solution();
+        int depth = sol.maxDepth(root);
+
+        System.out.println("Maximum Depth: " + depth);
+    }
 }
 class Solution {
     public int maxDepth(TreeNode root) {
