6500df35cd1dd7d4dae7fff231658624491f6d63
Rahul Mulik <rahulmulik9@gmail.com>
2026-03-29 10:55:53 +0530

    Added solution

--- src/Challenge75/Day51/PathSum.java
+++ src/Challenge75/Day51/PathSum.java
@@ -1,4 +1,38 @@
 package Challenge75.Day51;
 
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+
+    }
+}
+
 public class PathSum {
+    public static void main(String[] args) {
+
+    }
 }
