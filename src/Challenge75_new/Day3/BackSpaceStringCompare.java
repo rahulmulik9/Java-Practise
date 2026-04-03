74d1e430042128ff76a3dca2fdfd3bcc0ced90ea
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-03 16:57:34 +0530

    Added solutions class

--- src/Challenge75_new/Day3/BackSpaceStringCompare.java
+++ src/Challenge75_new/Day3/BackSpaceStringCompare.java
@@ -1,4 +1,19 @@
 package Challenge75_new.Day3;
+/*
+* Imagine you're typing on a keyboard. Sometimes you hit backspace which deletes the last letter you typed.
+* In this problem, # means backspace.s = "ab#c"
+→ type a → "a"
+→ type b → "ab"
+→ hit #  → "a"   (b deleted)
+→ type c → "ac"
+t = "ad#c"
+→ type a → "a"
+→ type d → "ad"
+→ hit #  → "a"   (d deleted)
+→ type c → "ac"
 
+Both give "ac" → return true ✅*/
+
+class SolBackSpaceStringCompare{}
 public class BackSpaceStringCompare {
 }
