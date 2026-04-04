f9c76610f89af9e0efd3c6952995f63680b82a04
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-04 15:56:42 +0530

    Brute Force

--- src/Challenge75_new/Day4/MaximumSubArray.java
+++ src/Challenge75_new/Day4/MaximumSubArray.java
@@ -1,4 +1,22 @@
 package Challenge75_new.Day4;
 
+class SolMaximumSubArray{
+    public int maxSubArrayBrute(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentSum = 0;
+            for (int j = i; j < nums.length; j++) {
+                currentSum += nums[j];
+                maxSum = Math.max(maxSum, currentSum);
+            }
+        }
+        return maxSum;
+    }
+}
+
 public class MaximumSubArray {
+    public static void main(String[] args) {
+
+    }
 }
