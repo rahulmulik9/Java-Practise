a1b9b8bac83ce3fa8e79186a74d009f018746375
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-06 08:52:47 +0530

    🔨 Brute Force

--- src/Challenge75_new/Day6/LongestSubstringWithoutRepeatingCharacters.java
+++ src/Challenge75_new/Day6/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,22 @@
 package Challenge75_new.Day6;
 
+class SolLongestSubstringWithoutRepeatingCharacters {
+
+    public static int lengthOfLongestSubstringBrute(String s) {
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                boolean allUnique = true;
+                boolean[] seen = new boolean[128];
+                for (int k = i; k < j; k++) {
+                    if (seen[s.charAt(k)]) { allUnique = false; break; }
+                    seen[s.charAt(k)] = true;
+                }
+                if (allUnique) maxLen = Math.max(maxLen, j - i);
+            }
+        }
+        return maxLen;
+    }
+}
 public class LongestSubstringWithoutRepeatingCharacters {
 }
