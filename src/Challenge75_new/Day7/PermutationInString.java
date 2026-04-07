869c11a0acdd6a0331acf386a7d501d2d55a0409
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-07 09:32:08 +0530

    🔨 Brute Force

--- src/Challenge75_new/Day7/PermutationInString.java
+++ src/Challenge75_new/Day7/PermutationInString.java
@@ -1,4 +1,21 @@
 package Challenge75_new.Day7;
 
+
+import java.util.Arrays;
+
+class SolPermutationInString {
+
+    public static boolean checkInclusionBrute(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        char[] sorted1 = s1.toCharArray();
+        Arrays.sort(sorted1);
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            char[] sub = s2.substring(i, i + s1.length()).toCharArray();
+            Arrays.sort(sub);
+            if (Arrays.equals(sorted1, sub)) return true;
+        }
+        return false;
+    }
+}
 public class PermutationInString {
 }
