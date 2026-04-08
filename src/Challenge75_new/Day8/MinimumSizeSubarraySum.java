56bbd83e664e9f5eabef795f77c5b789b562a537
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-08 09:53:45 +0530

    ✅ Optimal Solution

--- src/Challenge75_new/Day8/MinimumSizeSubarraySum.java
+++ src/Challenge75_new/Day8/MinimumSizeSubarraySum.java
@@ -19,6 +19,23 @@ class SolMinimumSizeSubarraySum {
 
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
+    public static int minSubArrayLenOptimal(int target, int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+
+            while (sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
 }
 public class MinimumSizeSubarraySum {
 }
