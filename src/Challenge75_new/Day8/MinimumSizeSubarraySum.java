de8c615d9e0d61ca963e9e154a12c74184a48aa9
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-08 09:53:13 +0530

    🔨 Brute Force

--- src/Challenge75_new/Day8/MinimumSizeSubarraySum.java
+++ src/Challenge75_new/Day8/MinimumSizeSubarraySum.java
@@ -1,4 +1,24 @@
 package Challenge75_new.Day8;
 
+class SolMinimumSizeSubarraySum {
+
+    public static int minSubArrayLenBrute(int target, int[] nums) {
+        int n = nums.length;
+        int minLen = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum >= target) {
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
+                }
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+}
 public class MinimumSizeSubarraySum {
 }
