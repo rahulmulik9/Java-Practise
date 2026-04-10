1330f3b93c0cdb6292de53b558c22ff177f364e7
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-10 09:25:42 +0530

    🔨 Brute Force

--- src/Challenge75_new/Day10/SubarrayProductLessThanK.java
+++ src/Challenge75_new/Day10/SubarrayProductLessThanK.java
@@ -1,4 +1,20 @@
 package Challenge75_new.Day10;
+class SolSubarrayProductLessThanK {
 
+    public static int numSubarrayProductLessThanKBrute(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int product = 1;
+            for (int j = i; j < nums.length; j++) {
+                product *= nums[j];
+                if (product < k) count++;
+                else break;
+            }
+        }
+        return count;
+    }
+
+}
 public class SubarrayProductLessThanK {
 }
