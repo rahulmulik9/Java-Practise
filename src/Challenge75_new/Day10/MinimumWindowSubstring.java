c800e073605f7a98087f9bee4e4372b477297c5e
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-11 16:50:12 +0530

    ✅ Optimal Solution

--- src/Challenge75_new/Day10/MinimumWindowSubstring.java
+++ src/Challenge75_new/Day10/MinimumWindowSubstring.java
@@ -18,6 +18,28 @@ class SolMinimumWindowSubstring {
         }
         return result;
     }
+    public static String minWindowOptimal(String s, String t) {
+        if (s.length() < t.length()) return "";
+        int[] need = new int[128];
+        int[] have = new int[128];
+        for (char c : t.toCharArray()) need[c]++;
+        int required = 0;
+        for (int f : need) if (f > 0) required++;
+        int formed = 0, left = 0, minLen = Integer.MAX_VALUE, start = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            have[c]++;
+            if (need[c] > 0 && have[c] == need[c]) formed++;
+            while (formed == required) {
+                if (right - left + 1 < minLen) { minLen = right - left + 1; start = left; }
+                char l = s.charAt(left);
+                have[l]--;
+                if (need[l] > 0 && have[l] < need[l]) formed--;
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
 }
 
 public class MinimumWindowSubstring {
