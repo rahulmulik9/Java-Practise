d271a51779869b60ff014c0527c9c16a91c85b2e
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-12 08:37:38 +0530

    🔨 Brute Force

--- src/Challenge75_new/Day11/LongestRepeatingCharacterReplacement.java
+++ src/Challenge75_new/Day11/LongestRepeatingCharacterReplacement.java
@@ -1,4 +1,21 @@
 package Challenge75_new.Day11;
+class SolLongestRepeatingCharacterReplacement {
 
+    public static int characterReplacementBrute(String s, int k) {
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int[] freq = new int[26];
+            int maxFreq = 0;
+            for (int j = i; j < s.length(); j++) {
+                freq[s.charAt(j) - 'A']++;
+                maxFreq = Math.max(maxFreq, freq[s.charAt(j) - 'A']);
+                int windowSize = j - i + 1;
+                if (windowSize - maxFreq <= k) maxLen = Math.max(maxLen, windowSize);
+                else break;
+            }
+        }
+        return maxLen;
+    }
+}
 public class LongestRepeatingCharacterReplacement {
 }
