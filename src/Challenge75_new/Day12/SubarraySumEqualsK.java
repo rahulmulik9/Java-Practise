00913a935d71b8956b4eb7dc4e34496f0ddd7885
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-13 09:10:06 +0530

    🔨 Brute Force

---
+++ src/Challenge75_new/Day12/SubarraySumEqualsK.java
@@ -0,0 +1,18 @@
+package Challenge75_new.Day12;
+class SolSubarraySumEqualsK {
+
+    public static int subarraySumBrute(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) count++;
+            }
+        }
+        return count;
+    }
+}
+
+public class SubarraySumEqualsK {
+}
