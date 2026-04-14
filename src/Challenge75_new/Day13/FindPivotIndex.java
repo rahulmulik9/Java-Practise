fdc25d3d636032e9f2d5f664b5e5a6dc7e55ad9e
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-14 09:05:40 +0530

    🔨 Brute Force

--- src/Challenge75_new/Day13/FindPivotIndex.java
+++ src/Challenge75_new/Day13/FindPivotIndex.java
@@ -1,4 +1,15 @@
 package Challenge75_new.Day13;
+class SolFindPivotIndex {
 
+    public static int pivotIndexBrute(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int leftSum = 0, rightSum = 0;
+            for (int j = 0; j < i; j++)        leftSum  += nums[j];
+            for (int j = i + 1; j < nums.length; j++) rightSum += nums[j];
+            if (leftSum == rightSum) return i;
+        }
+        return -1;
+    }
+}
 public class FindPivotIndex {
 }
