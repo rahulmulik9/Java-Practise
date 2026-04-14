2db6cab76e675cc2ca084a4e5092fe23bddb39f8
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-14 09:06:05 +0530

    ✅ Optimal Solution

--- src/Challenge75_new/Day13/FindPivotIndex.java
+++ src/Challenge75_new/Day13/FindPivotIndex.java
@@ -10,6 +10,17 @@ class SolFindPivotIndex {
         }
         return -1;
     }
+    public static int pivotIndexOptimal(int[] nums) {
+        int total = 0;
+        for (int num : nums) total += num;
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = total - leftSum - nums[i];
+            if (leftSum == rightSum) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
 }
 public class FindPivotIndex {
 }
