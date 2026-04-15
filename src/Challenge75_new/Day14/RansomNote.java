e7cf23aed6161c67dd1bd59fe4ea05958e77737e
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-15 07:32:07 +0530

    ✅ Optimal Solution

--- src/Challenge75_new/Day14/RansomNote.java
+++ src/Challenge75_new/Day14/RansomNote.java
@@ -1,4 +1,16 @@
 package Challenge75_new.Day14;
 
+class SolRansomNote {
+
+    public static boolean canConstructBrute(String ransomNote, String magazine) {
+        StringBuilder sb = new StringBuilder(magazine);
+        for (char c : ransomNote.toCharArray()) {
+            int idx = sb.indexOf(String.valueOf(c));
+            if (idx == -1) return false;
+            sb.deleteCharAt(idx);
+        }
+        return true;
+    }
+}
 public class RansomNote {
 }
