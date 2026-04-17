bd2f5b58e2f6635ff9ccfc7b952066c7025e709e
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-17 08:39:10 +0530

    Optimum solutions

--- src/Challenge75_new/Day16/LongestConsecutiveSequence.java
+++ src/Challenge75_new/Day16/LongestConsecutiveSequence.java
@@ -1,6 +1,8 @@
 package Challenge75_new.Day16;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class SolLongestConsecutiveSequence {
     public static int longestConsecutiveBrute(int[] nums) {
@@ -12,6 +14,18 @@ class SolLongestConsecutiveSequence {
             if (nums[i] == nums[i - 1] + 1) curLen++;
             else curLen = 1;
             maxLen = Math.max(maxLen, curLen);
+        }
+        return maxLen;
+    }
+    public static int longestConsecutiveOptimal(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+        int maxLen = 0;
+        for (int n : set) {
+            if (set.contains(n - 1)) continue;
+            int cur = n, len = 1;
+            while (set.contains(cur + 1)) { cur++; len++; }
+            maxLen = Math.max(maxLen, len);
         }
         return maxLen;
     }
