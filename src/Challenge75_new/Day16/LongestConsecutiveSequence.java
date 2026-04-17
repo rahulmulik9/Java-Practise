2a8985963e9206699210cab0743021a60855df1e
Rahul Mulik <rahulmulik9@gmail.com>
2026-04-17 08:38:30 +0530

    Brute force

--- src/Challenge75_new/Day16/LongestConsecutiveSequence.java
+++ src/Challenge75_new/Day16/LongestConsecutiveSequence.java
@@ -1,4 +1,21 @@
 package Challenge75_new.Day16;
 
+import java.util.Arrays;
+
+class SolLongestConsecutiveSequence {
+    public static int longestConsecutiveBrute(int[] nums) {
+        if (nums.length == 0) return 0;
+        Arrays.sort(nums);
+        int maxLen = 1, curLen = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) continue;
+            if (nums[i] == nums[i - 1] + 1) curLen++;
+            else curLen = 1;
+            maxLen = Math.max(maxLen, curLen);
+        }
+        return maxLen;
+    }
+}
+
 public class LongestConsecutiveSequence {
 }
