edf4d03e838b768e904dc46c33705f3a30aa13bd
Rahul Mulik <rahulmulik9@gmail.com>
2026-07-06 12:24:44 +0530

    Added solutions

--- src/Challenge75_DSA/July/Day6/TwoSum.java
+++ src/Challenge75_DSA/July/Day6/TwoSum.java
@@ -1,4 +1,28 @@
 package Challenge75_DSA.July.Day6;
 
+
+class SolTwoSum {
+
+    public static int[] twoSumBrute(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++)
+            for (int j = i + 1; j < nums.length; j++)
+                if (nums[i] + nums[j] == target) return new int[]{i, j};
+        return new int[]{};
+    }
+
+    public static int[] twoSumOptimal(int[] nums, int target) {
+        java.util.Map<Integer, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) return new int[]{map.get(complement), i};
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
+
 public class TwoSum {
+    public static void main(String[] args) {
+
+    }
 }
