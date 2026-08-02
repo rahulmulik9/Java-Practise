48a8ed7cd6835a58b3fa60522fbd7d89faed9649
rahulmulik9 <rahulmulik9@gmail.com>
2026-08-02 18:20:23 +0530

    Solutions added

---
+++ src/Challenge75_DSA/August/Day1/Day2/LongesSubSrring.java
@@ -0,0 +1,19 @@
+package Challenge75_DSA.August.Day1.Day2;
+
+public class LongesSubSrring {
+}
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        java.util.Map<Character, Integer> lastIndex = new java.util.HashMap<>();
+        int max = 0, left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (lastIndex.containsKey(c) && lastIndex.get(c) >= left) {
+                left = lastIndex.get(c) + 1;
+            }
+            lastIndex.put(c, right);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
