ee5365ae355577be76046f2cf73793c080fb5c9a
rahulmulik9 <rahulmulik9@gmail.com>
2026-08-13 18:42:56 +0530

    Solutions added

---
+++ src/Challenge75_DSA/August/Day13/FIndDuplicate.java
@@ -0,0 +1,25 @@
+package Challenge75_DSA.August.Day13;
+
+public class FIndDuplicate {
+}
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[nums[0]];
+        int fast = nums[nums[nums[0]]];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
