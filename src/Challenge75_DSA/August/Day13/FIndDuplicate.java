e4a20aa84adcfcb625f1db10cd4bf43d4b8deeb9
rahulmulik9 <rahulmulik9@gmail.com>
2026-08-15 22:22:51 +0530

    Solutions added

--- src/Challenge75_DSA/August/Day13/FIndDuplicate.java
+++ src/Challenge75_DSA/August/Day13/FIndDuplicate.java
@@ -1,5 +1,9 @@
 package Challenge75_DSA.August.Day13;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public class FIndDuplicate {
 }
 
@@ -22,4 +26,22 @@ class Solution {
 
         return slow;
     }
+
+    Function<String, String> myFunction = (s)->s.toUpperCase();
+
+    String toUpperCaseMySelf(String s){
+        return s.toUpperCase();
+    }
+
+    void toTheStuff(){
+        String randomName = "asdfasd";
+        String getUpperByFunction = myFunction.apply(randomName);
+        String getUpperByMethod = toUpperCaseMySelf(randomName);
+
+        List<String> mylist = List.of(new String[]{"asdf", "asdfasd", "asdfsdfg", "wert"});
+        List<String> resulted =mylist.stream().map(s -> toUpperCaseMySelf(s)).toList();
+        List<String> resulted2 =mylist.stream().map(s -> myFunction.apply(s)).toList();
+    }
+
+
 }
