b1d1d6b87401475020737c1be2e765cfc91cbcd7
rahulmulik9 <rahulmulik9@gmail.com>
2026-08-16 14:58:31 +0530

    Solutions added

---
+++ src/Challenge75_DSA/August/Day1/ReverseLinkedList.java
@@ -0,0 +1,28 @@
+package Challenge75_DSA.August.Day1;
+
+public class ReverseLinkedList {
+}
+
+
+   class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;  // save next before breaking link
+            curr.next = prev;           // reverse the link
+            prev = curr;                // move prev forward
+            curr = next;                // move curr forward
+        }
+        return prev;
+    }
+}
